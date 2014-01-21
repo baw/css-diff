026f41549e40886efa05e278322b5db2e75e4d84
Brian <iamamthwiz+GitHub@gmail.com>
2014-01-21 01:30:38 -0500

    added 'concat' and 'qunit' tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,26 @@
 module.exports = function (grunt) {
-    
+
     //Project configuration
     grunt.initConfig({
-        "jshint" : {
+        "jshint": {
             "files": {
                 "src": ["src/*.js"]
             }
+        },
+        "concat": {
+            dist: {
+                src: ['src/*.js'],
+                dest: 'build/build.js'
+            }
+        },
+        "qunit": {
+            files: ['test/qunit/tests.html']
         }
     });
-    
+
     grunt.loadNpmTasks('grunt-contrib-jshint');
-    
-    grunt.registerTask('test', ['jshint']);
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-qunit');
+
+    grunt.registerTask('test', ['jshint', 'concat', 'qunit']);
 };
