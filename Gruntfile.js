c85a3c1cfca525f4ba9762d528ead6ef35635a2e
Brian <iamamthwiz+GitHub@gmail.com>
2014-01-23 20:03:36 -0500

    added 'grunt-contrib-watch' plugin

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,12 +17,19 @@ module.exports = function (grunt) {
         },
         "qunit": {
             files: ['test/qunit/tests.html']
+        },
+        "watch": {
+            "test": {
+                files: ['test/qunit/js/tests.js', 'test/qunit/tests.html', 'src/*.js'],
+                tasks: ['test']
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-qunit');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
     grunt.registerTask('test', ['jshint', 'concat', 'qunit']);
 };
