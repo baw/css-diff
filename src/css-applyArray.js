aa5ea91e8c09f96c474852f1db6eb447024ed55b
Brian <iamamthwiz+GitHub@gmail.com>
2014-03-04 22:07:02 -0500

    bug fix

--- src/css-applyArray.js
+++ src/css-applyArray.js
@@ -1,3 +1,5 @@
+/*jslint plusplus:true, vars:true */
+
 /*
     applyArray.js
     © 2014 Brian Weiser. All rights reserved.
@@ -6,19 +8,31 @@
 var applyArray;
 (function () {
     
-    
-    var processMediaQuery = function (cssArray) {
+    /*
+        Returns end results of css based on given media query
+    */
+    var processMediaQuery = function (cssArray, mediaQuery) {
+        var c;
+        for (c = 0; c < cssArray.length; c++) {
+            
+        }
         
     };
     
     applyArray = function (cssArray) {
-        var mediaArray = [];
-        for (var c = 0; i < cssArray.length; c++) {
+        var mediaArray = [], c, m, i;
+        var selectorObj = {};
+        
+        for (c = 0; c < cssArray.length; c++) {
+            for (i = 0; i < cssArray[c].innerCSS.length; i++) {
+                selectorObj[cssArray[c].innerCSS[i]] = cssArray[c].innerCSS[i];
+            }
+        }
+        
+        for (m = 0; m < mediaArray.length; m++) {
             
         }
         
-        for (var m = 0; i < mediaArray.length; m++) {
-            
-        }
+        return mediaArray;
     };
 }());
